a035f96134236b7d88d97050d2381456730821b4
Aymn ibrahim <Aymnibrahim730@gmail.com>
2023-09-06 00:05:33 +0300

    Created The Main Screen

---
+++ Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Test_in_C__Windows_framework_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtEnglish = new System.Windows.Forms.TextBox();
+            this.txtMorse = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Test_in_C__Windows_framework_.Properties.Resources.photo_2023_09_04_01_55_39__2_;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(243, 560);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txtEnglish
+            // 
+            this.txtEnglish.BackColor = System.Drawing.Color.Teal;
+            this.txtEnglish.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEnglish.Location = new System.Drawing.Point(388, 139);
+            this.txtEnglish.Multiline = true;
+            this.txtEnglish.Name = "txtEnglish";
+            this.txtEnglish.Size = new System.Drawing.Size(349, 166);
+            this.txtEnglish.TabIndex = 1;
+            // 
+            // txtMorse
+            // 
+            this.txtMorse.BackColor = System.Drawing.Color.Teal;
+            this.txtMorse.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMorse.Location = new System.Drawing.Point(388, 324);
+            this.txtMorse.Multiline = true;
+            this.txtMorse.Name = "txtMorse";
+            this.txtMorse.Size = new System.Drawing.Size(349, 166);
+            this.txtMorse.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LemonChiffon;
+            this.ClientSize = new System.Drawing.Size(1078, 584);
+            this.Controls.Add(this.txtMorse);
+            this.Controls.Add(this.txtEnglish);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Morse Code";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtEnglish;
+        private System.Windows.Forms.TextBox txtMorse;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Test_in_C__Windows_framework_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtEnglish.BackColor = System.Drawing.Color.FromArgb(249, 253, 213);
+            txtMorse.BackColor = System.Drawing.Color.FromArgb  (249, 253, 213);
+
+        }
+
+
+        public static string LetterToMorse(string letter)
+        {
+            switch (letter.ToLower())
+            {
+                case "a":
+                    return ".-";
+                case "b":
+                    return "-...";
+                case "c":
+                    return "-.-.";
+                case "d":
+                    return "-..";
+                case "e":
+                    return ".";
+                case "f":
+                    return "..-.";
+                case "g":
+                    return "--.";
+                case "h":
+                    return "....";
+                case "i":
+                    return "..";
+                case "j":
+                    return ".---";
+                case "k":
+                    return "-.-";
+                case "l":
+                    return ".-..";
+                case "m":
+                    return "--";
+                case "n":
+                    return "-.";
+                case "o":
+                    return "---";
+                case "p":
+                    return ".--.";
+                case "q":
+                    return "--.-";
+                case "r":
+                    return ".-.";
+                case "s":
+                    return "...";
+                case "t":
+                    return "-";
+                case "u":
+                    return "..-";
+                case "v":
+                    return "...-";
+                case "w":
+                    return ".--";
+                case "x":
+                    return "-..-";
+                case "y":
+                    return "-.--";
+                case "z":
+                    return "--..";
+
+                case "1":
+                    return ".--.";
+                case "2":
+                    return "..-..";
+                case "3":
+                    return "...-.";
+                case "4":
+                    return "....-";
+                case "5":
+                    return "---";
+                case "6":
+                    return "......";
+                case "7":
+                    return "--..";
+                case "8":
+                    return "-....";
+                case "9":
+                    return "-..-";
+                case "0":
+                    return "____"; // Special case
+
+                case " ":
+                    return " / "; // to sperate between words
+                   
+                default:
+                    return "Worng Letter";
+            }
+        }
+
+
+    }
+}
